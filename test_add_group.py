5085306268491bb1aeb8fe9e8a95fdab654b28bf
Андреева Анастасия <a.andreeva@office.ngs.ru>
2020-03-21 14:20:35 +0700

    Refactor test add group

--- test_add_group.py
+++ test_add_group.py
@@ -2,8 +2,7 @@
 import unittest
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from group import Group
 
 
 class AddTestGroup(unittest.TestCase):
@@ -11,29 +10,45 @@ class AddTestGroup(unittest.TestCase):
         self.dw = webdriver.Chrome()
         self.dw.implicitly_wait(30)
 
-    def test_add_group(self):
-        dw = self.dw
+    def open_groups_page(self, dw):
         dw.get("http://localhost:8080/addressbook/group.php")
-        dw.find_element_by_name("user").send_keys("admin")
-        dw.find_element_by_name("pass").send_keys("secret")
+
+    def login(self, dw, username, password):
+        dw.find_element_by_name("user").send_keys(username)
+        dw.find_element_by_name("pass").send_keys(password)
         dw.find_element_by_xpath("//input[@value='Login']").click()
+
+    def create_group(self, dw, group):
+        # init group creation
         dw.find_element_by_name("new").click()
-        dw.find_element_by_name("group_name").send_keys("dddd")
-        dw.find_element_by_name("group_header").send_keys("dddd")
-        dw.find_element_by_name("group_footer").send_keys("dddd")
+        # fill group form
+        dw.find_element_by_name("group_name").send_keys(group.name)
+        dw.find_element_by_name("group_header").send_keys(group.header)
+        dw.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
         dw.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, dw):
         dw.find_element_by_link_text("groups").click()
+
+    def logout(self, dw):
         dw.find_element_by_link_text("Logout").click()
 
-    def is_element_present(self, how, what):
-        try: self.dw.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
+    def test_add_group(self):
+        dw = self.dw
+        self.open_groups_page(dw)
+        self.login(dw, username="admin", password="secret")
+        self.create_group(dw, Group(name="dddd", header="dddd", footer="dddd"))
+        self.return_to_groups_page(dw)
+        self.logout(dw)
 
-    def is_alert_present(self):
-        try: self.dw.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+    def test_add_empty_group(self):
+        dw = self.dw
+        self.open_groups_page(dw)
+        self.login(dw, username="admin", password="secret")
+        self.create_group(dw, Group(name="", header="", footer=""))
+        self.return_to_groups_page(dw)
+        self.logout(dw)
 
     def tearDown(self):
         self.dw.quit()
