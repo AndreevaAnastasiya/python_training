e434eb4736afeacda369c0f818fb212c85c1b0b0
Андреева Анастасия <a.andreeva@office.ngs.ru>
2020-03-28 18:59:12 +0700

    Added conftest and test for delete group

--- fixture/group.py
+++ fixture/group.py
@@ -4,12 +4,27 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def create(self, group):
+    def open_group_page(self):
         dw = self.app.dw
         dw.find_element_by_link_text("groups").click()
+
+    def return_to_groups_page(self):
+        dw = self.app.dw
+        dw.find_element_by_link_text("group page").click()
+
+    def create(self, group):
+        dw = self.app.dw
+        self.open_group_page()
         dw.find_element_by_name("new").click()
         dw.find_element_by_name("group_name").send_keys(group.name)
         dw.find_element_by_name("group_header").send_keys(group.header)
         dw.find_element_by_name("group_footer").send_keys(group.footer)
         dw.find_element_by_name("submit").click()
-        dw.find_element_by_link_text("groups").click()
+        self.return_to_groups_page()
+
+    def delete_first(self):
+        dw = self.app.dw
+        self.open_group_page()
+        dw.find_element_by_name("selected[]").click()
+        dw.find_element_by_name("delete").click()
+        self.return_to_groups_page()
