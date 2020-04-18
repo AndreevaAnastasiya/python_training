29f1fd62654db65f056d28f5b766468b2738367d
Андреева Анастасия <a.andreeva@office.ngs.ru>
2020-04-18 23:35:33 +0700

    Added choise browser

--- conftest.py
+++ conftest.py
@@ -1,16 +1,19 @@
 import pytest
+
 from fixture.application import Application
 
 fixture = None
 
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseurl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     elif not fixture.is_valid():
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -23,3 +26,9 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseurl", action="store",
+                     default="http://localhost:8080/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -6,11 +6,20 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.dw = webdriver.Firefox
+        elif browser == "chrome":
+            self.dw = webdriver.Chrome
+        elif browser == "ie":
+            self.dw = webdriver.Ie
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.dw = webdriver.Chrome()
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -18,6 +27,10 @@ class Application:
             return True
         except:
             return False
+
+    def open_home_page(self):
+        dw = self.dw
+        dw.get(self.base_url)
 
     def destroy(self):
         self.dw.quit()
