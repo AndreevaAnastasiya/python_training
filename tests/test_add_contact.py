dceed11bcf93b242e755c81baa68306b0b690752
Андреева Анастасия <a.andreeva@office.ngs.ru>
2020-04-27 11:34:47 +0700

    Added load data from json

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,32 +1,9 @@
 # -*- coding: utf-8 -*-
 from models.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(maxlen):
-    symbols = string.ascii_letters + string.digits
-    return "".join(
-        [random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_phones(maxlen):
-    symbols = string.digits + "-" + "(" + ")"
-    return "".join(
-        [random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-    Contact(firstname=random_string(7), lastname=random_string(10),
-            home=random_phones(10), work=random_phones(10),
-            email=random_string(10), mobile=random_phones(10),
-            phone2=random_phones(10), address=random_string(15),
-            email2=random_string(10), email3=random_string(10)) for i in range(3)]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
